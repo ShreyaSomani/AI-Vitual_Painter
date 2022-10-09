11ffc003d12c20ef4ab2ef867a6ce197eb323900
Shreya Somani <57498575+ShreyaSomani@users.noreply.github.com>
2022-10-09 13:58:52 +0530

    Update Hand-Tracking.py

--- Hand-Tracking.py
+++ Hand-Tracking.py
@@ -1,114 +1,83 @@
 import cv2
 import mediapipe as mp
 import time
-import math
-import numpy as np
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2,modelComplexity=1, detectionCon=0.5, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
+        self.modelComplex = modelComplexity
         self.detectionCon = detectionCon
         self.trackCon = trackCon
-
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(self.mode, self.maxHands,
-        self.detectionCon, self.trackCon)
-        self.mpDraw = mp.solutions.drawing_utils
-        self.tipIds = [4, 8, 12, 16, 20]
+        self.hands = self.mpHands.Hands(self.mode, self.maxHands,self.modelComplex,self.detectionCon, self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils #drawing lines
+        self.tipIds = [4, 8, 12, 16, 20] #thumb, index, middle, ring, pinky
 
-def findHands(self, img, draw=True):
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    self.results = self.hands.process(imgRGB)
-    # print(results.multi_hand_landmarks)
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #hands take in only rgb image
+        self.results = self.hands.process(imgRGB)
+        #print(self.results.multi_hand_landmarks)
+        if self.results.multi_hand_landmarks: #draw points - (21)
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms,self.mpHands.HAND_CONNECTIONS) #draw lines connecting each landmark
+        return img
 
-    if self.results.multi_hand_landmarks:
-        for handLms in self.results.multi_hand_landmarks:
-            if draw:
-                self.mpDraw.draw_landmarks(img, handLms,
-                self.mpHands.HAND_CONNECTIONS)
+    def findPosition(self, img, handNo=0, draw=True):
+        self.lmList = []
 
-    return img
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]#info of particular hand number
+            for id, lm in enumerate(myHand.landmark): #id of each landmark and landmark(x,y,z)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h) #find pos of landmark on image
+                self.lmList.append([id, cx, cy])
 
-def findPosition(self, img, handNo=0, draw=True):
-    xList = []
-    yList = []
-    bbox = []
-    self.lmList = []
-    if self.results.multi_hand_landmarks:
-        myHand = self.results.multi_hand_landmarks[handNo]
-        for id, lm in enumerate(myHand.landmark):
-            # print(id, lm)
-            h, w, c = img.shape
-            cx, cy = int(lm.x * w), int(lm.y * h)
-            xList.append(cx)
-            yList.append(cy)
-            # print(id, cx, cy)
-            self.lmList.append([id, cx, cy])
-            if draw:
-                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
+                if draw:
+                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED) #radius, rgb color, fill ; filling pos of landmark on hand with color
 
-    xmin, xmax = min(xList), max(xList)
-    ymin, ymax = min(yList), max(yList)
-    bbox = xmin, ymin, xmax, ymax
+        return self.lmList
 
-    if draw:
-        cv2.rectangle(img, (xmin - 20, ymin - 20), (xmax + 20, ymax + 20),
-        (0, 255, 0), 2)
+    def fingersUp(self):
+        fingers = []
+        # Thumb - FOR RIGHT HAND
 
-    return self.lmList, bbox
-
-def fingersUp(self):
-    fingers = []
-    # Thumb
-    if self.lmList[self.tipIds[0]][1] > self.lmList[self.tipIds[0] - 1][1]:
-        fingers.append(1)
-    else:
-        fingers.append(0)
-
-    # Fingers
-    for id in range(1, 5):
-        if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
-            fingers.append(1)
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1) #right - opened
         else:
-            fingers.append(0)
+            fingers.append(0) #left - closed
 
-        # totalFingers = fingers.count(1)
 
-    return fingers
+        # Fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]: #tip of finger above other landmark which is 2 steps below it
+                fingers.append(1) #finger opened - above the other landmark
+            else:
+                fingers.append(0) #finger closed - below
 
-def findDistance(self, p1, p2, img, draw=True,r=15, t=3):
-    x1, y1 = self.lmList[p1][1:]
-    x2, y2 = self.lmList[p2][1:]
-    cx, cy = (x1 + x2) // 2, (y1 + y2) // 2
+        totalFingers = fingers.count(1)
+        return fingers
 
-    if draw:
-        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), t)
-        cv2.circle(img, (x1, y1), r, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (x2, y2), r, (255, 0, 255), cv2.FILLED)
-        cv2.circle(img, (cx, cy), r, (0, 0, 255), cv2.FILLED)
-        length = math.hypot(x2 - x1, y2 - y1)
-
-    return length, img, [x1, y1, x2, y2, cx, cy]
 
 def main():
-    pTime = 0
-    cTime = 0
-    cap = cv2.VideoCapture(1)
+    pTime = 0 #past time
+    cTime = 0 #current time
+    cap = cv2.VideoCapture(0) #0 or 1
     detector = handDetector()
+
     while True:
         success, img = cap.read()
         img = detector.findHands(img)
-        lmList, bbox = detector.findPosition(img)
+        lmList = detector.findPosition(img)
         if len(lmList) != 0:
             print(lmList[4])
 
         cTime = time.time()
-        fps = 1 / (cTime - pTime)
+        fps = 1 / (cTime - pTime) #frames per second
         pTime = cTime
 
-        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,
-        (255, 0, 255), 3)
+        cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3,(255, 0, 255), 3) #pos,font,scale,rgb color of frame notif,thickness
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
